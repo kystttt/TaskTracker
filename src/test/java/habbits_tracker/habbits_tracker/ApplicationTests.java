2339f98e4219c925f20b01adddc87134cd64d6c8
Leonid Sevostianov <doghoper75@gmail.com>
2025-12-31 13:48:44 +0500

    initial commit

---
+++ src/test/java/habbits_tracker/habbits_tracker/ApplicationTests.java
@@ -0,0 +1,13 @@
+package habbits_tracker.habbits_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
