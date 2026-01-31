ac823c23be3b217aa964cc264f9d59ecf6640e9f
Leonid <146662933+kystttt@users.noreply.github.com>
2026-01-31 21:46:17 +0500

    remove trash

--- src/main/java/habbits_tracker/habbits_tracker/secure/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package habbits_tracker.habbits_tracker.secure.repository;
-
-import habbits_tracker.habbits_tracker.secure.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
--- src/main/java/habbits_tracker/habbits_tracker/secure/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package habbits_tracker.habbits_tracker.secure.repository;
-
-import habbits_tracker.habbits_tracker.secure.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
